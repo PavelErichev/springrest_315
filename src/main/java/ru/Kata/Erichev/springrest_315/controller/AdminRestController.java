1a4057f25beaf92344abeae5bb5afac82529f92d
PavelErichev <dustfud@yandex.ru>
2023-03-06 23:09:40 +0300

    Commit_4

--- src/main/java/ru/Kata/Erichev/springrest_315/controller/AdminRestController.java
+++ src/main/java/ru/Kata/Erichev/springrest_315/controller/AdminRestController.java
@@ -52,38 +52,4 @@ public class AdminRestController {
         userService.deleteUser(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
-
-   /* @GetMapping
-    public String showAllUser(@AuthenticationPrincipal User user, Model model) {
-        List<User> allUsers = userService.getAllUsers();
-        model.addAttribute("allUs", allUsers);
-        model.addAttribute("user", user);
-        return "admin";
-    }
-
-    @GetMapping("/user-create")
-    public String addNewUser(@AuthenticationPrincipal User user, Model model) {
-        User newUser = new User();
-        model.addAttribute("newUser", newUser);
-        model.addAttribute("user", user);
-        return "user-create";
-    }
-
-    @PostMapping("/user-create")
-    public String saveUser(@ModelAttribute("user") User user, @RequestParam(value = "roless") String[] role) {
-        userService.saveUser(user, role);
-        return "redirect:/admin";
-    }
-
-    @DeleteMapping("/user-delete/{id}")
-    public String deleteUser(@PathVariable("id") long id) {
-        userService.deleteUser(id);
-        return "redirect:/admin";
-    }
-
-    @PutMapping(value = "/user-update/{id}")
-    public String updateUser(@ModelAttribute("user") User user, @RequestParam(value = "roless") String[] role) {
-        userService.saveUser(user, role);
-        return "redirect:/admin";
-    }*/
 }
