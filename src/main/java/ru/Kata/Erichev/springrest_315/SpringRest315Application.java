4df44cf52e56754724954e021c9bb6fd28345810
PavelErichev <dustfud@yandex.ru>
2023-03-06 22:19:16 +0300

    Commit_2

---
+++ src/main/java/ru/Kata/Erichev/springrest_315/SpringRest315Application.java
@@ -0,0 +1,13 @@
+package ru.Kata.Erichev.springrest_315;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRest315Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRest315Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/Kata/Erichev/springrest_315/Springrest315ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.Kata.Erichev.springrest_315;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springrest315ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
